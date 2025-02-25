a49f78e44e91aabf36e5b5b43ca499096082ee6e
Zlodeyanin <evgeniy_zavarnitsin@mail.ru>
2025-02-25 18:20:52 +0300

    Добавьте файлы проекта.

---
+++ NSSplit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NSSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Привет. Меня зовут Евгений. Я прохожу курс.";
+            string[] words = text.Split(' ');
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
